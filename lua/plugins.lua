91aad91173b9eb7f235ef92ca1793bdda31298f0
Troy-Kettle <troykettle19@gmail.com>
2025-02-03 12:42:03 +0000

    added autopairing

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,16 +1,26 @@
 return require('packer').startup(function(use)
-    -- Plug ins 
+    -- Plugin manager
     use 'wbthomason/packer.nvim'
+
+    -- Plugins
     use 'github/copilot.vim'
     use 'nvim-treesitter/nvim-treesitter'
     use 'tpope/vim-commentary'
     use 'neovim/nvim-lspconfig'
     use {
         'nvim-telescope/telescope.nvim',
-        requires = { {'nvim-lua/plenary.nvim'} }
+        requires = { 'nvim-lua/plenary.nvim' }
+    }
+    
+    -- Auto pairs
+    use {
+        "windwp/nvim-autopairs",
+        config = function()
+            require("nvim-autopairs").setup {}
+        end
     }
 
-    -- colorscheme
+    -- Colorscheme
     use 'craftzdog/solarized-osaka.nvim' -- Solarized Osaka colorscheme
-    
 end)
+
