6c8f11aaf769570e088bf075eb534fc7ea8147b1
Troy-Kettle <troykettle19@gmail.com>
2025-02-03 11:34:37 +0000

    added python lsp

--- init.lua
+++ init.lua
@@ -29,3 +29,6 @@ vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
 vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
 vim.keymap.set('n', '<leader>fh', '<cmd>Telescope help_tags<cr>')
 
+local lspconfig = require('lspconfig')
+
+lspconfig.pyright.setup{}
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,13 +1,16 @@
 return require('packer').startup(function(use)
-    -- Existing plugins
+    -- Plug ins 
     use 'wbthomason/packer.nvim'
     use 'github/copilot.vim'
     use 'nvim-treesitter/nvim-treesitter'
     use 'tpope/vim-commentary'
+    use 'neovim/nvim-lspconfig'
     use {
         'nvim-telescope/telescope.nvim',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
+
+    -- colorscheme
     use 'craftzdog/solarized-osaka.nvim' -- Solarized Osaka colorscheme
     
 end)
