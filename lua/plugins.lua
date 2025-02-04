0bfd12f645ecc055da599baa4fd2758fcb7d37fb
Troy-Kettle <troykettle19@gmail.com>
2025-02-04 12:29:34 +0000

    added treesitter for syntax highlighting - only core langauges

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,14 +4,61 @@ return require('packer').startup(function(use)
 
     -- Plugins
     use 'github/copilot.vim'
-    use 'nvim-treesitter/nvim-treesitter'
     use 'tpope/vim-commentary'
     use 'neovim/nvim-lspconfig'
     use {
         'nvim-telescope/telescope.nvim',
         requires = { 'nvim-lua/plenary.nvim' }
     }
-    
+
+    -- TreeSitter
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate',
+        config = function()
+            require('nvim-treesitter.configs').setup {
+                -- A list of parser names, or "all"
+                ensure_installed = {
+                    "lua",
+                    "vim",
+                    "python",
+                    "javascript",
+                    "typescript",
+                    "json",
+                    "html",
+                    "css",
+                },
+
+                -- Install parsers synchronously (only applied to `ensure_installed`)
+                sync_install = false,
+
+                -- Automatically install missing parsers when entering buffer
+                auto_install = true,
+
+                highlight = {
+                    -- `false` will disable the whole extension
+                    enable = true,
+
+                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                    additional_vim_regex_highlighting = false,
+                },
+
+                indent = { enable = true },
+
+                -- Optional: Enable incremental selection
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "gnn",
+                        node_incremental = "grn",
+                        scope_incremental = "grc",
+                        node_decremental = "grm",
+                    },
+                },
+            }
+        end
+    }
+
     -- Auto pairs
     use {
         "windwp/nvim-autopairs",
@@ -23,4 +70,3 @@ return require('packer').startup(function(use)
     -- Colorscheme
     use 'craftzdog/solarized-osaka.nvim' -- Solarized Osaka colorscheme
 end)
-
