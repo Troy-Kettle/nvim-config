7f61b6938cba70fae3bca150c93b2832c41ef300
Troy-Kettle <troykettle19@gmail.com>
2025-02-05 21:40:23 +0000

    fixed the broken new colorscheme working now

--- init.lua
+++ init.lua
@@ -1,67 +1,47 @@
 
--- Set leader key FIRST!
-vim.g.mapleader = " "
+-- Force-load packer.nvim (this should be the very first line)
+vim.cmd [[packadd packer.nvim]]
 
--- General Options
-vim.opt.number = true                -- Show line numbers
-vim.opt.relativenumber = true        -- Relative line numbers
-vim.opt.expandtab = true             -- Use spaces instead of tabs
-vim.opt.tabstop = 4                  -- Spaces per tab
-vim.opt.shiftwidth = 4               -- Indentation size
-vim.opt.smartindent = true           -- Auto-indent new lines
-vim.opt.wrap = false                 -- Disable line wrap
-vim.opt.ignorecase = true            -- Case-insensitive search...
-vim.opt.smartcase = true             -- ...unless uppercase is used
-vim.opt.termguicolors = true         -- True color support
-vim.opt.background = "dark"          -- Dark background
+-- Basic Editor Settings
+vim.opt.number = true               -- Enable line numbers
+vim.opt.relativenumber = true       -- Relative line numbers
+vim.opt.expandtab = true            -- Use spaces instead of tabs
+vim.opt.shiftwidth = 4              -- Indent size
+vim.opt.tabstop = 4                 -- Tab width
+vim.opt.smartindent = true          -- Smart autoindenting
+vim.opt.wrap = false                -- Disable line wrap
+vim.opt.termguicolors = true        -- Enable true colors
+vim.opt.background = "dark"         -- Set dark background
 
 -- Load Plugins (from lua/plugins.lua)
-require("plugins")
+require('plugins')
 
--- Catppuccin Colorscheme Setup (with lualine integration disabled)
-require("catppuccin").setup({
-    flavour = "mocha",               -- Options: "mocha", "macchiato", "frappe", "latte"
-    transparent_background = false,
-    term_colors = true,
-    integrations = {
-        lualine = false,             -- Disabled to prevent the error
-        telescope = true,
-        treesitter = true,
-        which_key = true,
-        indent_blankline = true,
-    },
-})
-vim.cmd("colorscheme catppuccin")
+-- Colorscheme Setup: Try to load 'catppuccin'; if not available, fallback to 'desert'
+local status, _ = pcall(vim.cmd, "colorscheme catppuccin")
+if not status then
+    vim.cmd("colorscheme desert")
+end
 
--- Telescope Keymaps
+-- Setup lualine (Statusline)
+if pcall(require, "lualine") then
+    require("lualine").setup {
+        options = {
+            theme = "auto",
+            section_separators = "",
+            component_separators = "|",
+        },
+    }
+end
+
+-- Telescope Keymaps (Fuzzy Finder)
 vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Find Files" })
 vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>",  { desc = "Live Grep" })
 vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>",    { desc = "Buffers" })
 vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>",  { desc = "Help Tags" })
 
--- LSP Setup (example: Pyright for Python)
-local lspconfig = require("lspconfig")
-lspconfig.pyright.setup {}
-
--- UI Enhancements
-
--- Statusline: lualine using its own configuration
-require("lualine").setup {
-    options = {
-        theme = "catppuccin",  -- Using Catppuccin theme (if available) or choose another
-        section_separators = "",
-        component_separators = "│",
-    },
-}
-
--- Which-key for keybinding hints
-require("which-key").setup {}
-
--- Indent Guides: indent-blankline configuration
-vim.opt.list = true
-vim.opt.listchars:append "space:⋅"  -- Optional: show spaces as dots
-require("indent_blankline").setup {
-    char = "│",
-    show_trailing_blankline_indent = false,
-}
+-- LSP Configuration (Example: Pyright for Python)
+local lspconfig_status, lspconfig = pcall(require, "lspconfig")
+if lspconfig_status then
+    lspconfig.pyright.setup {}
+end
 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,78 +1,42 @@
 
-return require("packer").startup(function(use)
-    -- Plugin Manager
+return require('packer').startup(function(use)
+    -- Let packer manage itself
     use "wbthomason/packer.nvim"
 
     -- Colorscheme: Catppuccin
     use { "catppuccin/nvim", as = "catppuccin" }
 
-    -- File icons for statusline, etc.
-    use "kyazdani42/nvim-web-devicons"
-
-    -- Statusline: lualine
+    -- Statusline: lualine (with icon support)
     use {
         "nvim-lualine/lualine.nvim",
-        requires = { "kyazdani42/nvim-web-devicons" },
+        requires = { "kyazdani42/nvim-web-devicons" }
     }
 
-    -- Which-key for keybinding hints
+    -- Which-key: Popup for key bindings
     use "folke/which-key.nvim"
 
-    -- Indent Guides: indent-blankline
-    use "lukas-reineke/indent-blankline.nvim"
-    -- If you need to pin an older version, you can do something like:
-    -- use { "lukas-reineke/indent-blankline.nvim", commit = "YOUR_DESIRED_COMMIT_HASH" }
+   -- Telescope: Fuzzy Finder
+    use {
+        "nvim-telescope/telescope.nvim",
+        requires = { "nvim-lua/plenary.nvim" }
+    }
 
-    -- GitHub Copilot
-    use "github/copilot.vim"
-
-    -- Commentary for commenting out code
-    use "tpope/vim-commentary"
+    -- Treesitter: Enhanced syntax highlighting and code parsing
+    use {
+        "nvim-treesitter/nvim-treesitter",
+        run = ":TSUpdate"
+    }
 
     -- LSP Configurations
     use "neovim/nvim-lspconfig"
 
-    -- Telescope: Fuzzy Finder
-    use {
-        "nvim-telescope/telescope.nvim",
-        requires = { "nvim-lua/plenary.nvim" },
-    }
+    -- Autopairs: Automatically insert matching brackets, quotes, etc.
+    use "windwp/nvim-autopairs"
 
-    -- Treesitter for better syntax highlighting and code understanding
-    use {
-        "nvim-treesitter/nvim-treesitter",
-        run = ":TSUpdate",
-        config = function()
-            require("nvim-treesitter.configs").setup {
-                ensure_installed = {
-                    "lua", "vim", "python", "javascript", "typescript", "json", "html", "css"
-                },
-                sync_install = false,
-                auto_install = true,
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = false,
-                },
-                indent = { enable = true },
-                incremental_selection = {
-                    enable = true,
-                    keymaps = {
-                        init_selection = "gnn",
-                        node_incremental = "grn",
-                        scope_incremental = "grc",
-                        node_decremental = "grm",
-                    },
-                },
-            }
-        end,
-    }
+    -- GitHub Copilot
+    use "github/copilot.vim"
 
-    -- Auto pairs: Automatically insert matching brackets, quotes, etc.
-    use {
-        "windwp/nvim-autopairs",
-        config = function()
-            require("nvim-autopairs").setup {}
-        end,
-    }
+    -- Commentary: Easy commenting
+    use "tpope/vim-commentary"
 end)
 
