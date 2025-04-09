3235235772a7a0fef6bcd955c44e86455bdb310c
Troy-Kettle <troykettle19@gmail.com>
2025-04-09 14:24:10 +0100

    weird issues

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
--- space before any mappings
+-- Space before any mappings
 vim.g.mapleader = " "
 -- Force-load packer.nvim (this should be the very first line for plugins)
 vim.cmd [[packadd packer.nvim]]
@@ -15,6 +15,7 @@ vim.opt.background = "dark"         -- Set dark background
 vim.g.netrw_list_hide = '.*\\.swp$'  -- Hide swap files in netrw
 -- Load Plugins (from lua/plugins.lua)
 require('plugins')
+
 -- Configure rose-pine
 require('rose-pine').setup({
     dark_variant = 'main',
@@ -29,11 +30,13 @@ require('rose-pine').setup({
         Statement = { bold = true },
     },
 })
+
 -- Try to load rose-pine; if not available, fallback to desert
 local status, _ = pcall(vim.cmd, "colorscheme rose-pine")
 if not status then
     vim.cmd("colorscheme desert")
 end
+
 -- Setup lualine (Statusline)
 if pcall(require, "lualine") then
     require("lualine").setup {
@@ -44,15 +47,74 @@ if pcall(require, "lualine") then
         },
     }
 end
--- Telescope Keymaps (Fuzzy Finder)
-vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Find Files" })
-vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>",  { desc = "Live Grep" })
-vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>",    { desc = "Buffers" })
-vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>",  { desc = "Help Tags" })
--- LSP Configuration (Example: Pyright for Python)
-local lspconfig_status, lspconfig = pcall(require, "lspconfig")
-if lspconfig_status then
-    lspconfig.pyright.setup {}
+
+-- Directly set up Python LSP (without using newer plugins)
+local lsp_status, _ = pcall(require, "lspconfig")
+if lsp_status then
+    -- Setup Python LSP (pyright)
+    require('lspconfig').pyright.setup {
+        on_attach = function(client, bufnr)
+            -- Enable completion triggered by <c-x><c-o>
+            vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+            -- Mappings
+            local opts = { noremap = true, silent = true }
+            vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+            vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+            vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+            vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+            vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+            vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+            vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+            vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+            vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+        end,
+        settings = {
+            python = {
+                analysis = {
+                    autoSearchPaths = true,
+                    diagnosticMode = "workspace",
+                    useLibraryCodeForTypes = true
+                }
+            }
+        }
+    }
 end
--- init.lua
-vim.keymap.set("i", "<CapsLock>", "<Esc>")  -- in insert mode, caps lock = escape
+
+-- Basic nvim-cmp setup (if available)
+local cmp_status, cmp = pcall(require, "cmp")
+if cmp_status then
+    cmp.setup({
+        mapping = {
+            ['<C-Space>'] = cmp.mapping.complete(),
+            ['<CR>'] = cmp.mapping.confirm({ select = true }),
+            ['<Tab>'] = cmp.mapping.select_next_item(),
+            ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+        },
+        sources = {
+            { name = 'nvim_lsp' },
+            { name = 'buffer' },
+        }
+    })
+end
+
+-- Telescope Keymaps (Fuzzy Finder)
+vim.api.nvim_set_keymap("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { noremap = true, silent = true })
+
+-- CapsLock to Escape in insert mode (Note: This may not work in all terminals)
+vim.api.nvim_set_keymap("i", "<CapsLock>", "<Esc>", { noremap = true })
+
+-- Set up which-key if available
+local whichkey_status, whichkey = pcall(require, "which-key")
+if whichkey_status then
+    whichkey.setup {}
+end
+
+-- Setup nvim-autopairs if available
+local autopairs_status, autopairs = pcall(require, "nvim-autopairs")
+if autopairs_status then
+    autopairs.setup {}
+end
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,47 +1,50 @@
-
 return require('packer').startup(function(use)
     -- Let packer manage itself
     use "wbthomason/packer.nvim"
-
-    -- Colorscheme: Catppuccin
-    use { "catppuccin/nvim", as = "catppuccin" }
     
-    use 'rose-pine/neovim'
-
-    -- Inside your plugins.lua file, add:
-    use 'rebelot/kanagawa.nvim'
-
+    -- Colorschemes
+    use { "catppuccin/nvim", as = "catppuccin" }
+    use 'rose-pine/neovim'  -- Without the 'as' parameter to match existing installation
+    use "rebelot/kanagawa.nvim"
+    
     -- Statusline: lualine (with icon support)
     use {
         "nvim-lualine/lualine.nvim",
         requires = { "kyazdani42/nvim-web-devicons" }
     }
-
+    
     -- Which-key: Popup for key bindings
     use "folke/which-key.nvim"
-
-   -- Telescope: Fuzzy Finder
-   use {
+    
+    -- Telescope: Fuzzy Finder
+    use {
         'nvim-telescope/telescope.nvim',
         requires = { {'nvim-lua/plenary.nvim'} }
-    }    -- Treesitter: Enhanced syntax highlighting and code parsing
-
+    }
+    
+    -- Treesitter: Enhanced syntax highlighting and code parsing
     use {
         "nvim-treesitter/nvim-treesitter",
         run = ":TSUpdate"
     }
-
-    -- LSP Configurations
-    use "neovim/nvim-lspconfig"
-
+    
+    -- LSP Configuration (compatible with Neovim 0.9.5)
+    use {
+        "neovim/nvim-lspconfig",
+        tag = "v0.1.3"  -- Use an older version compatible with Neovim 0.9.5
+    }
+    
+    -- Basic autocompletion for Neovim 0.9
+    use "hrsh7th/nvim-cmp"
+    use "hrsh7th/cmp-nvim-lsp"
+    use "hrsh7th/cmp-buffer"
+    
     -- Autopairs: Automatically insert matching brackets, quotes, etc.
     use "windwp/nvim-autopairs"
-
+    
     -- GitHub Copilot
     use "github/copilot.vim"
-
+    
     -- Commentary: Easy commenting
     use "tpope/vim-commentary"
-
 end)
-
